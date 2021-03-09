a63b75bd9af6c55103675b1c26f428da09f4bbef
Ramil Safin <RamilSafNab1996@gmail.com>
2021-03-09 11:25:37 +0300

    Add std containers in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+#include <deque>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
 int main(int argc, char **argv) {
   std::cout << "Hello, stranger!" << std::endl;
+
+  auto array_list = vector<int>();
+  auto linked_list = list<int>();
+  auto cpp_queue = queue<int>();
+  auto cpp_deque = deque<int>();
+  auto cpp_stack = stack<int>();
+
   return 0;
 }
